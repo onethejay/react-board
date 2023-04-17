daca278905d3e0b465a14388458d72169fa17920
onethejay <jhjang951@gmail.com>
2023-04-17 22:55:04 +0900

    chap3 업로드

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import BoardList from "./routes/BoardList";
+import BoardDetail from "./routes/BoardDetail";
+import Home from "./routes/Home";
+import Header from "./layout/Header";
+import React from "react";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/board" element={<BoardList/>}/>
+        <Route path="/board/:idx" element={<BoardDetail/>}/>
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
---
+++ frontend/src/routes/BoardDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BoardDetail = () => {
+  return (
+    <div>
+      게시판 상세 출력
+    </div>
+  );
+};
+
+export default BoardDetail;

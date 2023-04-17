0770fcb243de5969ddcf221df02990c34fc28ce2
onethejay <jhjang951@gmail.com>
2023-04-17 23:09:35 +0900

    chap3 업로드

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
 import {Route, Routes} from "react-router-dom";
 import BoardList from "./routes/BoardList";
-import BoardDetail from "./routes/BoardDetail";
 import Home from "./routes/Home";
-import Header from "./layout/Header";
 import React from "react";
-import Footer from "./layout/Footer";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/board" element={<BoardList/>}/>
-        <Route path="/board/:idx" element={<BoardDetail/>}/>
-      </Routes>
-      <Footer/>
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/board" element={<BoardList/>}/>
+    </Routes>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <Header/>
     <App/>
+    <Footer/>
   </BrowserRouter>
 );

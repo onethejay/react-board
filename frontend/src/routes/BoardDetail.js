a707af43d759e06a6ec5d7125d128234387c7579
onethejay <jhjang951@gmail.com>
2023-04-18 23:12:16 +0900

    chap4 업로드

---
+++ frontend/src/routes/BoardDetail.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Board from '../components/Board';
+
+const BoardDetail = () => {
+  const { idx } = useParams(); // /board/:idx와 동일한 변수명으로 데이터를 꺼낼 수 있습니다.
+  const [loading, setLoading] = useState(true);
+  const [board, setBoard] = useState({});
+  const getBoard = async () => {
+    const resp = await (await axios.get(`//localhost:8080/board/${idx}`)).data;
+    setBoard(resp.data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getBoard();
+  });
+
+  return (
+    <div>
+      {loading ? (
+        <h2>loading...</h2>
+      ) : (
+        <Board
+          idx={board.idx}
+          title={board.title}
+          contents={board.contents}
+          createdBy={board.createdBy}
+        />
+      )}
+    </div>
+  );
+};
+
+export default BoardDetail;
--- frontend/src/routes/BoardList.js
+++ frontend/src/routes/BoardList.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const BoardList = () => {
+  const [boardList, setBoardList] = useState([]);
+
+  const getBoardList = async () => {
+    const resp = await (await axios.get('//localhost:8080/board')).data; // 2) 게시글 목록 데이터에 할당
+    setBoardList(resp.data); // 3) boardList 변수에 할당
+
+    const pngn = resp.pagination;
+    console.log(pngn);
+  };
+
+  useEffect(() => {
+    getBoardList(); // 1) 게시글 목록 조회 함수 호출
+  }, []);
+
   return (
     <div>
-      게시판 목록 출력
+      <ul>
+        {boardList.map((board) => (
+          // 4) map 함수로 데이터 출력
+          <li key={board.idx}>
+            <Link to={`/board/${board.idx}`}>{board.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };

1941dabe3741a3687694979335c2f0136eb880a1
onethejay <jhjang951@gmail.com>
2023-05-08 23:00:55 +0900

    페이징, 검색 구현

--- frontend/src/routes/BoardList.js
+++ frontend/src/routes/BoardList.js
@@ -8,18 +8,26 @@ const BoardList = () => {
   const [boardList, setBoardList] = useState([]);
   const [pageList, setPageList] = useState([]);
 
-  const [curPage, setCurPage] = useState(1); //현재 페이지 세팅
+  const [curPage, setCurPage] = useState(0); //현재 페이지 세팅
   const [prevBlock, setPrevBlock] = useState(0); //이전 페이지 블록
   const [nextBlock, setNextBlock] = useState(0); //다음 페이지 블록
   const [lastPage, setLastPage] = useState(0); //마지막 페이지
 
-  const getBoardList = async (page) => {
-    if (page === curPage) return; //현재 페이지와 누른 페이지가 같으면 return
+  const [search, setSearch] = useState({
+    page: 1,
+    sk: '',
+    sv: '',
+  });
 
-    if (!page) page = 1;
+  const getBoardList = async () => {
+    if (search.page === curPage) return; //현재 페이지와 누른 페이지가 같으면 return
+
+    const queryString = Object.entries(search)
+      .map((e) => e.join('='))
+      .join('&');
 
     const resp = await (
-      await axios.get('//localhost:8080/board?page=' + page)
+      await axios.get('//localhost:8080/board?' + queryString)
     ).data; // 2) 게시글 목록 데이터에 할당
 
     setBoardList(resp.data); // 3) boardList 변수에 할당
@@ -27,7 +35,7 @@ const BoardList = () => {
 
     const { endPage, nextBlock, prevBlock, startPage, totalPageCnt } = pngn;
 
-    setCurPage(page);
+    setCurPage(search.page);
     setPrevBlock(prevBlock);
     setNextBlock(nextBlock);
     setLastPage(totalPageCnt);
@@ -46,12 +54,36 @@ const BoardList = () => {
 
   const onClick = (event) => {
     let value = event.target.value;
-    getBoardList(value);
+    setSearch({
+      ...search,
+      page: value,
+    });
+
+    getBoardList();
+  };
+
+  const onChange = (event) => {
+    const { value, name } = event.target; //event.target에서 name과 value만 가져오기
+    setSearch({
+      ...search,
+      [name]: value,
+    });
+  };
+
+  const onSearch = () => {
+    if (search.sk !== '' && search.sv !== '') {
+      setSearch({
+        ...search,
+        page: 1,
+      });
+      setCurPage(0);
+      getBoardList();
+    }
   };
 
   useEffect(() => {
     getBoardList(); // 1) 게시글 목록 조회 함수 호출
-  }, []);
+  }, [search]);
 
   return (
     <div>
@@ -81,6 +113,16 @@ const BoardList = () => {
         <button onClick={onClick} value={lastPage}>
           &gt;&gt;
         </button>
+      </div>
+      <br />
+      <div>
+        <select name="sk" onChange={onChange}>
+          <option value="">-선택-</option>
+          <option value="title">제목</option>
+          <option value="contents">내용</option>
+        </select>
+        <input type="text" name="sv" id="" onChange={onChange} />
+        <button onClick={onSearch}>검색</button>
       </div>
       <br />
       <div>
